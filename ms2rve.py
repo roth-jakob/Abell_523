d6a8f7be61c495739c8505a77708f4063089c371
Julian Ruestig <jruestig@posteo.de>
2024-05-07 11:44:02 +0200

    master: ms2rve split spw from 1-3

--- ms2rve.py
+++ ms2rve.py
@@ -19,11 +19,11 @@ outdir = args.output_dir
 makedirs(outdir, exist_ok=True)
 
 
-spw = 1
-
 name = input.split('/')[-1] + '_fld{field:02d}_spw{spw}'
-obs = rve.ms2observations(input, "DATA", True, spw)
-for ii, o in enumerate(obs):
-    if o is None:
-        continue
-    o.save(join(outdir, name.format(field=ii, spw=spw)), False)
+
+for spw in range(1, 4):
+    obs = rve.ms2observations(input, "DATA", True, spw)
+    for ii, o in enumerate(obs):
+        if o is None:
+            continue
+        o.save(join(outdir, name.format(field=ii, spw=spw)), False)
