b5ab7651f55293ccc4044c00b1e64c67c1c5c314
Julian Ruestig <jruestig@posteo.de>
2024-05-29 09:32:28 +0200

    mfreq: make mfreq possible

--- mosaic_imaging.py
+++ mosaic_imaging.py
@@ -38,7 +38,8 @@ center_dec = cfg['sky']['image center dec']
 center_frame = cfg['sky']['image center frame']
 npix = int(cfg['sky']['space npix x'])
 fov = float(cfg['sky']['space fov x'].split('d')[0]) * u.deg
-output_directory = f"output/{base}_{cfg['Output']['output name']}_{npix}"
+output_directory = f"output/{base}_{cfg['Output']['output name']}_{npix}_test"
+print('Output:', output_directory)
 
 sky, sky_diffuse_operators = rve.sky_model_diffuse(cfg['sky'])
 pdom, tdom, fdom, sdom = sky.target
@@ -59,6 +60,8 @@ data_filenames = [join(path, f'{base}.ms_fld{ii:02}_spw00.npz')
 # data_filenames = [join(path, f'{base}.ms_fld08_spw{jj:02}.npz')
 #                   for jj in range(3)]
 
+# data_filenames = [join(path, f'{base}.ms_fld{ii:02}_spw{jj:02}.npz')
+#                   for ii in range(5, 11) for jj in range(3)]
 
 all_obs = []
 for file in data_filenames:
@@ -68,15 +71,27 @@ for file in data_filenames:
         print('Restricting to stokes I')
         obs = obs.restrict_to_stokesi()
         obs = obs.average_stokesi()  # FIXME: Needs to be adjusted for polarization
+
     all_obs.append(obs)
+
+
+def build_filter_pointings(field_pointings):
+    def filter_pointings(obs):
+        if obs.direction in field_pointings:
+            return False
+
+        field_pointings.append(obs.direction)
+        return True
+    return filter_pointings
 
 
 sky_center = SkyCoord(center_ra, center_dec, unit=(
     u.hourangle, u.deg), frame=center_frame)
+
 sky_beamer = rve.build_sky_beamer(
     sky.target,
     sky_center,
-    all_obs,
+    [obs for obs in filter(build_filter_pointings([]), all_obs)],
     lambda freq, x: rve.alma_beam_func(D=25.0, d=1.0, freq=freq, x=x)
 )
 
@@ -103,34 +118,62 @@ def build_energy(response, obs):
     ) @ response
 
 
-responses = []
+responses_plotting = []
 energies = []
-for kk, vv, obs in zip(
-    sky_beamer._beam_directions.keys(),
-    sky_beamer._beam_directions.values(),
-    all_obs
-):
-    # R = build_response(kk, vv['dx'], vv['dy'], obs, tmp_sky.dtype)
-    R = build_response(
-        field_key=kk, dx=vv['dx'], dy=vv['dy'], obs=obs, domain=sky.target,
-        sky_dtype=sky_dtype)
-    responses.append(R @ sky_beamer)
-    energies.append(build_energy(R, obs))
+for o in all_obs:
+    for field_name, bd in sky_beamer._beam_directions.items():
+        if o.direction == bd['direction']:
+            R = build_response(field_name,
+                               dx=bd['dx'],
+                               dy=bd['dy'],
+                               obs=o,
+                               domain=sky.target,
+                               sky_dtype=sky_dtype)
+            responses_plotting.append(R @ sky_beamer)
+            energies.append(build_energy(R, o))
 
 
-for ii in range(1):
-    rnd = ift.from_random(sky.domain)
-    f = sky(rnd)
+rnd = ift.from_random(sky.domain)
+f = sky(rnd)
+tot_response_adjoint = np.zeros_like(f.val)
+fields = {}
+for ii, (rr, oo) in enumerate(zip(responses_plotting, all_obs)):
+    dirty = rr.adjoint(oo.vis).val
+    tot_response_adjoint += dirty
 
-    tot_response_adjoint = np.zeros_like(f.val)
-    for ii, (rr, oo) in enumerate(zip(responses, all_obs)):
-        dirty = rr.adjoint(oo.vis).val
-        plt.imshow(dirty[0, 0, 0], origin='lower')
-        plt.title(f'fldid={ii+5}')
+    for fn, bd in sky_beamer._beam_directions.items():
+        if oo.direction == bd['direction']:
+            fld = fn
+
+    if fld in fields:
+        fields[fld] = fields[fld] + dirty
+    else:
+        fields[fld] = dirty
+
+    if False:
+        fig, axes = plt.subplots(1, dirty.shape[2])
+        for ii, ax in enumerate(axes):
+            ax.imshow(dirty[0, 0, ii], origin='lower')
+            ax.set_title(f'{fld}: spb={ii}')
         plt.show()
-        tot_response_adjoint += dirty
-    plt.imshow(tot_response_adjoint[0, 0, 0], origin='lower')
+
+for fld, dirty in fields.items():
+    fig, axes = plt.subplots(1, dirty.shape[2])
+    if dirty.shape[2] == 1:
+        axes = [axes]
+    for ii, ax in enumerate(axes):
+        ax.imshow(dirty[0, 0, ii], origin='lower')
+        ax.set_title(f'{fld}: spb={ii}')
     plt.show()
+
+fig, axes = plt.subplots(1, dirty.shape[2])
+if dirty.shape[2] == 1:
+    axes = [axes]
+for ii, ax in enumerate(axes):
+    ax.imshow(tot_response_adjoint[0, 0, ii], origin='lower')
+    ax.set_title(f'combined: spb={ii}')
+plt.show()
+
 
 lh = reduce(lambda x, y: x+y, energies)
 lh = lh @ sky_beamer @ sky
@@ -138,9 +181,19 @@ lh = lh @ sky_beamer @ sky
 
 def callback(samples, i):
     sky_mean = samples.average(sky)
-    plt.imshow(sky_mean.val[0, 0, 0].T, origin="lower", norm=LogNorm())
-    # plt.contour(beam, levels=[0.1], colors='white')
-    plt.colorbar()
+    pols, ts, freqs, *_ = sky_mean.shape
+    fig, axes = plt.subplots(pols, freqs, figsize=(freqs, pols))
+
+    axes = [axes] if freqs == 1 else axes
+    axes = [axes] if pols == 1 else axes
+
+    for pol, pol_axes in enumerate(axes):
+        for freq, ax in enumerate(pol_axes):
+            im = ax.imshow(
+                sky_mean.val[pol, 0, freq].T, origin="lower", norm=LogNorm())
+            plt.colorbar(im, ax=ax)
+
+    plt.tight_layout()
     if master:
         plt.savefig(f"{output_directory}/resovle_iteration_{i}.png")
     plt.close()
